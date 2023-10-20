56af21bf8a56e765400ba273e66a120832e1ebc2
vpullv <84404308+vpullv@users.noreply.github.com>
2023-10-19 23:31:34 -0300

    agrega cambios character layout agrega imagen
    agregue el 2do formulario y tambien el png de aliado

---
+++ src/components/character.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import Message from './message';
+import aliado1 from '../imagenes/aliados/aliado.png';
+
+const CharacterComponent = () => {
+  const [showMessages, setShowMessages] = useState(true);
+
+  // Función para mostrar los mensajes
+  const toggleMessages = () => {
+    setShowMessages(!showMessages); // Cambia el valor actual al contrario
+  };
+
+  return (
+    <>
+      <div className="character" onClick={toggleMessages}>
+        {/* Mostrar la imagen de tu aliado */}
+        <img className='imagenaliado' src={aliado1} alt="Aliado 1" />
+      </div>
+      {showMessages && (
+        <div className="messages">
+          {/* Aquí debes colocar tus mensajes o el componente Message */}
+          <Message />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default CharacterComponent;
--- src/components/layout.js
+++ src/components/layout.js
@@ -10,6 +10,7 @@ import SaveComponent from './saveSessionComponent';
 import '../styles/stylesPP.css'
 import logo from '../imagenes/logo.png'; 
 import aliado from '../imagenes/aliados/aliado.png';
+import CharacterComponent from './character';
 
 const Layout = ({ children }) => {
 
@@ -20,6 +21,12 @@ const Layout = ({ children }) => {
     require('../imagenes/enemy1/PÉSCADO.png'),
     require('../imagenes/enemy1/SOMBRERIN.png'),
     require('../imagenes/enemy1/mirror.png'),
+  ];
+
+  const aliados = [
+    { nombre: 'pajaro', imagen: '../imagenes/aliados/aliado.png' },
+    { nombre: 'perro', imagen: 'ruta-aliado2.png' },
+    { nombre: 'gato', imagen: 'ruta-aliado3.png' },
   ];
 
   const session = {
@@ -246,7 +253,7 @@ const Layout = ({ children }) => {
           <p className='bienvenidaPlayer'>bienvenido {localStorage.getItem('User')}</p>
           <div className='reloj' id="reloj"></div>
           </header>
-            <Message />   
+              
             <div id="reloj"></div>
             <span id="enemyHealth" className='spanenemy'> Resistencia Emocional: {enemyInstance.vida}</span>
             
@@ -264,6 +271,7 @@ const Layout = ({ children }) => {
 
       </div>
       <div className="game-container">
+        <CharacterComponent/>
         <div className='enemycontainer'>
           {/* <div className="vidaenemigo">
             <span id="enemyHealth" className='spanenemy'> vida: {enemyInstance.vida}</span>
