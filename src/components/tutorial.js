9529944e4aa2f1931004c0c2ee19dd1175908a24
vpullv <84404308+vpullv@users.noreply.github.com>
2023-11-30 23:03:08 -0300

    agrega textos de tutorial

--- src/components/tutorial.js
+++ src/components/tutorial.js
@@ -36,19 +36,19 @@ function Tutorial({ children, flag , onClose}) {
     }
 
     if (nTuto === 2) {
-      setInstruction('Hola esta es la instruccion 2');
+      setInstruction('Al comenzar, veras un objetivo o desafio simple para superar, al hacer click lo superaras y respawneara uno nuevo');
     }
 
     if (nTuto === 3) {
-      setInstruction('Hola esta es la instruccion 3');
+      setInstruction('Al superar un desafio, te daran experiencia emocional y esta te servira para mejorar habilidades que te ayudaran a superar desafios de manera mas simple');
     }
 
     if (nTuto === 4) {
-      setInstruction('Hola esta es la instruccion 4');
+      setInstruction('En el borde se encuentra un boton para guardar partida , se descargara un archivo , guardalo muy bien para continuar con tu aventura');
     }
 
     if (nTuto === 5) {
-      setInstruction('Hola esta es la instruccion 5');
+      setInstruction('Abajo de guardar partida se encuentra un boton que activa la musica, presionalo y disfruta de Kokoro Healing');
     }
     // Esta función se ejecutará cada vez que nTuto cambie
 
